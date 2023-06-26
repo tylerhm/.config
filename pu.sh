8b882e2389189957649756febb47e71e3052a98a
Tyler Hostler-Mathis <tylerhm@meta.com>
2023-06-26 10:35:22 -0400

    Don't pu.sh LICENSE|README.md

--- pu.sh
+++ pu.sh
@@ -6,7 +6,7 @@ script=$(realpath "$0")
 dotsdir=$(dirname "$script")
 
 # Iterate over all files that aren't .git/* or this script
-find . | grep -Ev "^(\.|.*(\.git|pu\.sh).*)$" | while read -r file; do
+find . | grep -Ev "^(\.|.*(\.git|pu\.sh|LICENSE|README.md).*)$" | while read -r file; do
     if [[ ! -d $file ]]; then
         parentdir=$(dirname "$file")
         destdir="${home}/${parentdir}"

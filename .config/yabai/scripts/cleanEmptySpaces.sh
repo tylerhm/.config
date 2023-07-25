4ca32314cefad916c08e65a3cb5ad4b0886ff7d2
Tyler Hostler-Mathis <tylerhm.dev@gmail.com>
2023-07-24 22:09:23 -0400

    goodyabai 👋

--- .config/yabai/scripts/cleanEmptySpaces.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-yabai -m query --spaces --display | \
-    jq -re 'map(select(."is-native-fullscreen" == false)) | length > 1' \
-    && yabai -m query --spaces | \
-    jq -re 'map(select(."windows" == [] and ."has-focus" == false).index) | reverse | .[] ' | \
-    xargs -I % sh -c 'yabai -m space % --destroy' && \
-    osascript -e 'tell application id "tracesOf.Uebersicht" to refresh widget id "simple-bar-index-jsx"'

51542495959a96c7e80a4bbacd7e9541f193df76
Tyler Hostler-Mathis <tylerhm@meta.com>
2024-04-29 22:37:53 -0400

    Fix window binds

--- .config/nvim/lua/tylerhm/mappings.lua
+++ .config/nvim/lua/tylerhm/mappings.lua
@@ -115,10 +115,10 @@ if is_available "harpoon" then
 
   map("n", "<leader>a", function() harpoon:list():add() end)
   map("n", "<leader>j", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-  map("n", "<C-h>", function() harpoon:list():select(1) end)
-  map("n", "<C-j>", function() harpoon:list():select(2) end)
-  map("n", "<C-k>", function() harpoon:list():select(3) end)
-  map("n", "<C-l>", function() harpoon:list():select(4) end)
+  map("n", "<C-S-h>", function() harpoon:list():select(1) end)
+  map("n", "<C-S-j>", function() harpoon:list():select(2) end)
+  map("n", "<C-S-k>", function() harpoon:list():select(3) end)
+  map("n", "<C-S-l>", function() harpoon:list():select(4) end)
   map("n", "<C-S-P>", function() harpoon:list():prev() end)
   map("n", "<C-S-N>", function() harpoon:list():next() end)
 end

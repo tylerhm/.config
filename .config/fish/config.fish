85ac7a1781a605b37ad986d2c666c5d788a13a84
Tyler Hostler-Mathis <tylerhm.dev@gmail.com>
2024-09-09 21:03:56 -0400

    rbenv

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,3 +6,6 @@ if status is-interactive
     fnm env --shell fish --use-on-cd | source
     fnm completions --shell fish | source
 end
+
+# Added by `rbenv init` on Sat Aug  3 23:09:38 EDT 2024
+status --is-interactive; and rbenv init - --no-rehash fish | source
